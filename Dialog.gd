e3134fcad654f2ac31d6a877233d562f4a9bacc7
Kadema-FK <102257975+Kadema-FK@users.noreply.github.com>
2022-03-26 20:35:56 +0100

    Dodanie skryptu do dialogów

---
+++ Dialog.gd
@@ -0,0 +1,70 @@
+extends Node
+
+var scena1 = [
+	{
+		"Imie": "",
+		"Nastroj": "",
+		"Pozycja":"",
+		"Tekst":"Pojawiają się Chawa z Kifo.",
+		"Wybor": ["",""]
+	},	
+	{
+		"Imie": "Kifo",
+		"Nastroj": "Neutralny",
+		"Pozycja":"Prawo",
+		"Tekst":"Coś mówi Kifo.",
+		"Wybor": ["",""]
+	},	
+	{
+		"Imie": "Chawa",
+		"Nastroj": "Smutny",
+		"Pozycja":"Lewo",
+		"Tekst":"Chawa smutna.",
+		"Wybor": ["",""]
+	},	
+	{
+		"Imie": "",
+		"Nastroj": "",
+		"Pozycja":"",
+		"Tekst":"",
+		"Wybor": ["opcja1","opcja 2"]
+	},	
+	{
+		"Imie": "Chawa",
+		"Nastroj": "Smutny",
+		"Pozycja":"Lewo",
+		"Tekst":"Reakcja Chawa",
+		"Wybor": ["",""]
+	},
+	{
+		"Imie": "Kifo",
+		"Nastroj": "Neutralny",
+		"Pozycja":"Prawo",
+		"Tekst":"Reakcja Kifo. ",
+		"Wybor": ["",""]
+	}
+]
+
+#	{
+#		"Imie": "",
+#		"Nastroj": "",
+#		"Pozycja":"",
+#		"Tekst":"",
+#		"Wybor": ["",""]
+#	}
+
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
