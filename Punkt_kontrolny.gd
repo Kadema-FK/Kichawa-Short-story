afe4f3da7972903ce0f16005941c0a4d0c6d60c7
Kadema-FK <102257975+Kadema-FK@users.noreply.github.com>
2022-04-04 20:00:47 +0200

    Początek dialogu

---
+++ Punkt_kontrolny.gd
@@ -0,0 +1,41 @@
+extends Control
+
+onready var tekst = get_parent().get_node("Dialog").scena1
+
+var dialog_indeks = 0
+var zakonczone
+var aktywne
+
+var pozycja
+var nastroj
+
+func _ready():
+	wczytajDialog()
+
+func wczytajDialog():
+	if dialog_indeks < tekst.size():
+		aktywne = true
+		zakonczone = false
+		
+		$TextBox.visible = true
+		$TextBox/RichTextLabel.bbcode_text=tekst[dialog_indeks]["Tekst"]
+		$TextBox/Label.text = tekst[dialog_indeks]["Imie"]
+		
+		pozycja = tekst[dialog_indeks]["Pozycja"]
+		
+		$TextBox/RichTextLabel.percent_visible = 0
+		$TextBox/Tween.interpolate_property(
+			$TextBox/RichTextLabel, "percent_visible", 0, 1, 2,
+			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT
+		)
+		$TextBox/Tween.start()
+	else:
+		$TextBox.visible = false
+		zakonczone = true
+		aktywne = false
+	dialog_indeks += 1
+		
+
+
+func _on_Tween_tween_completed(_object, _key):
+	zakonczone = true
